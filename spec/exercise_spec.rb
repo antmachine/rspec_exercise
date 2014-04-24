4f01385cb0f0738be201b723a99fc4a6bf6c257e
antmachine <anthonysallows@gmail.com>
2014-04-24 13:55:33 -0700

    Created a test for each operator option in the calculator.

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,3 +1,20 @@
 require './exercise'
 require 'spec_helper'
 
+describe 'calculator' do
+	it 'should add numbers if given a + sign' do
+		calculator(1, 4, '+').should eq(5)
+	end
+
+	it 'should subtract numbers if given a - sign' do
+		calculator(4, 1, '-').should eq(3)
+	end
+
+	it 'should multiply numbers if given a * sign' do
+		calculator(2, 4, '*').should eq(8)
+	end
+
+	it 'should divide numbers if given a / sign' do
+		calculator(20, 4, '/').should eq(5)
+	end
+end
